9c9663bef8e91b25922497dbfecf4fc2c7d5a144
fidbi-hasan <fidbihasan21@gmail.com>
2025-10-24 20:08:21 +0600

    study destination part added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,35 @@
-console.log('connected to index');
+// document.getElementById('card-single').addEventListener('mouseover', () => {
+//   document.getElementById('card-single').classList.add('hidden');
+//   document.getElementById('card-single-hover').classList.remove('hidden');
+// })
+
+// document.getElementById('card-single').addEventListener('mouseleave', () => {
+//   document.getElementById('card-single').classList.remove('hidden');
+//   document.getElementById('card-single-hover').classList.add('hidden');
+// })
+
+// const singleCards = document.querySelectorAll('.card-single');
+
+// singleCards.forEach((singleCard) => {
+//   singleCard.addEventListener('mouseover', () => {
+//     singleCard.classList.add('hidden');
+//   })
+// })
+
+const cardWrappers = document.querySelectorAll('.card-wrapper');
+
+cardWrappers.forEach((wrapper) => {
+  const normalCard = wrapper.querySelector('.card-single');
+  const hoverCard = wrapper.querySelector('.card-single-hover');
+
+  wrapper.addEventListener('mouseenter', () => {
+    normalCard.classList.add('hidden');
+    hoverCard.classList.remove('hidden');
+  });
+
+  wrapper.addEventListener('mouseleave', () => {
+    normalCard.classList.remove('hidden');
+    hoverCard.classList.add('hidden');
+  });
+});
+
